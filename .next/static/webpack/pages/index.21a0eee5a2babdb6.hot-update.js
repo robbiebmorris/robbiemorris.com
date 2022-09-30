b99c63e554244f39ce120957bb60945236bcb710
robbiebmorris <robbiebmorris@gmail.com>
2022-09-30 14:02:18 +0100

    init

---
+++ .next/static/webpack/pages/index.21a0eee5a2babdb6.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('{"readmeUrl":"https://raw.githubusercontent.com/cveinnt/cveinnt/master/README.md","title":"robbiemorris.com","name":"Robbie Morris","ascii":"95% Awake","social":{"github":"github","linkedin":"linkedin","instagram":"Instagram","youtube":"Youtube"},"email":"robbiebmorris@gmail.com","ps1_hostname":"robbiemorris.com","ps1_username":"visitor","repo":"https://github.com/robbiebmorris","resume_url":"https://upload.wikimedia.org/wikipedia/commons/c/cc/Resume.pdf","donate_urls":{"paypal":"https://paypal.me/cveinnt","patreon":"https://patreon.com/cveinnt"},"colors":{"light":{"background":"#FBF1C9","foreground":"#3C3836","yellow":"#D79921","green":"#98971A","gray":"#7C6F64","blue":"#458588","red":"#CA2124"},"dark":{"background":"#0D0106","foreground":"#ffffff","yellow":"#d58747","green":"#A3BE8C","gray":"#88C0D0","blue":"#4dbdae","red":"#BF616A"}}}');
+
+/***/ })
+
+});
