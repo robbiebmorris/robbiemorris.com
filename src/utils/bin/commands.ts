d9bea79c86290d1a9afc34be26df31732495eb8f
robbiebmorris <robbiebmorris@gmail.com>
2022-11-15 09:15:19 +0000

    remove sudo

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -94,10 +94,10 @@ export const date = async (args: string[]): Promise<string> => {
   return new Date().toString();
 };
 
-export const sudo = async (args?: string[]): Promise<string> => {
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
-  return `Permission denied: nice try bro`;
-};
+// export const sudo = async (args?: string[]): Promise<string> => {
+//   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+//   return `Permission denied: nice try bro`;
+// };
 
 // Banner
 export const banner = (args?: string[]): string => {
